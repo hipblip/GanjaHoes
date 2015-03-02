51b2a529a8ae0c968da217d778bc4686d2b4cdd6
hipblip <sam.desranleau@gmail.com>
2015-03-02 16:37:11 -0500

    shit fuck

--- Driver.java
+++ Driver.java
@@ -14,16 +14,21 @@ public class Driver {
 	// When turn is even it's white's turn, when turn is odd it's black's turn
 	private static int turn = 0;
 	private static gameType playMode;
+	private static Scanner sc = new Scanner(System.in);
 	
+	// White is X
+	// Black is O
 	
 	public static void main(String[] args) {
 		
 		initBoard();		
-		intro();
+		//intro();
 		gamePause();
 		// Game Loop
 		while (true)
 		{
+			printBoard();
+			makeMove(turn);
 			break;
 		}
 		
@@ -32,7 +37,7 @@ public class Driver {
 	
 	static void intro()
 	{
-		Scanner sc = new Scanner(System.in);
+		
 		int response;
 		
 		System.out.println("Welcome to Ganja-Hoes!!\n");
@@ -50,35 +55,136 @@ public class Driver {
 			sc.nextLine();
 		}
 		
-		sc.close();
+		playMode = gameType.values()[response - 1];
+		
+		System.out.println("Playmode: " + playMode);
+		
 	}
 		
 	static void exit() 
 	{
+		sc.close();
 		System.out.println("Exiting game without error");
 		System.exit(0);
 	}
 	
 	static void gamePause() 
 	{
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		System.out.println("Press ENTER to continue...");
-		int whatthefuck = sc.nextInt();
-		sc.close();
+		sc.nextLine();
+		//sc.close();
 	}
 	
 	static void makeMove(int turn) 
 	{
-		Scanner sc = new Scanner(System.in);
+		String pos1;
+		String pos2;
+
 		if (turn%2 == 0) // White's turn
 		{
+			System.out.print("Please enter 2 consecutive vertical positions:\n1: ");
+			pos1 = sc.nextLine();
+			System.out.print("2: ");
+			pos2 = sc.nextLine();
+			
+			//check;
+			System.out.println(legalWhiteMove(pos1, pos2));
 			
 		}
 		else // Black's turn
 		{
-			
+			System.out.print("Please enter 2 consecutive horizontal positions:\n1: ");
+			pos1 = sc.nextLine();
+			System.out.print("2: ");
+			pos2 = sc.nextLine();
 		}
-		sc.close();
+	}
+	
+	static boolean legalBlackMove(String pos1, String pos2) 
+	{
+		char pos1Let = pos1.charAt(0);
+		char pos2Let = pos2.charAt(0);
+		int x1 = pos1Let - 65;
+		int x2 = pos2Let - 65;
+		int y1 = Integer.parseInt(pos1.charAt(1) + "") - 1;
+		int y2 = Integer.parseInt(pos2.charAt(1) + "") - 1;
+		
+		if (x1 > 7 || x1 < 0 || x2 > 7 || x2 < 0)
+		{
+			return false;
+		}
+		if (y1 > 7 || y1 < 0 || y2 > 7 || y2 < 0)
+		{
+			return false;
+		}
+		
+		if (gameBoard[x1][y1] != ' ' || gameBoard[x2][y2] != ' ' || x2 != x1) 
+		{
+			return false;
+		}
+		else 
+		{
+			if ((y1 == 0 && y2 != 1) || (y2 == 0 && y1 != 1)) 
+			{
+				return false;
+			}
+			else if ((y1 == 7 && y2 != 6) || (y2 == 7 && y1 != 6))
+			{
+				return false;
+			}			
+			else if (Math.abs(y1 - y2) > 1) 
+			{
+				return false;
+			}
+			else 
+			{
+				return true;
+			}
+		}
+	}
+	
+	static boolean legalWhiteMove(String pos1, String pos2) 
+	{
+		char pos1Let = pos1.charAt(0);
+		char pos2Let = pos2.charAt(0);
+		int x1 = pos1Let - 65;
+		int x2 = pos2Let - 65;
+		int y1 = Integer.parseInt(pos1.charAt(1) + "") - 1;
+		int y2 = Integer.parseInt(pos2.charAt(1) + "") - 1;
+		
+		if (x1 > 7 || x1 < 0 || x2 > 7 || x2 < 0)
+		{
+			return false;
+		}
+		if (y1 > 7 || y1 < 0 || y2 > 7 || y2 < 0)
+		{
+			return false;
+		}
+		
+		if (gameBoard[x1][y1] != ' ' || gameBoard[x2][y2] != ' ' || y2 != y1) 
+		{
+			return false;
+		}
+		else 
+		{
+			if ((x1 == 0 && x2 != 1) || (x2 == 0 && x1 != 1)) 
+			{
+				return false;
+			}
+			else if ((x1 == 7 && x2 != 6) || (x2 == 7 && x1 != 6))
+			{
+				return false;
+			}			
+			else if (Math.abs(x1 - x2) > 1) 
+			{
+				return false;
+			}
+			else 
+			{
+				return true;
+			}
+		}
 	}
 	
 	/*
@@ -122,6 +228,11 @@ public class Driver {
 	 * Gets and Sets
 	 *===============
 	 */
+	public int getTurn() 
+	{
+		return turn;
+	}
+	
 	public int getBoardX() 
 	{
 		return _BoardX;

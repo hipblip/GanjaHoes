7d6cdb1fe0a45caba121f810b84ef95236bac735
nuke <nuke.twoski@gmail.com>
2015-03-15 19:23:32 -0400

    java jamar abdul jabar

--- Minimax.java
+++ Minimax.java
@@ -42,6 +42,36 @@ public class Minimax {
 		return false;
 	}
 	
+	PlayablePair getPlayablePair(Coord c, boolean maximizingPlayer, Gameboard board)
+	{
+		try
+		{
+			if (maximizingPlayer)
+			{
+				if (board.getCharAt(c.getX() + 1, c.getY()) == ' ')
+				{
+					return new PlayablePair(c, new Coord(c.getX() + 1, c.getY()));
+				}
+				else 
+				{
+					return new PlayablePair(c, new Coord(c.getX() - 1, c.getY()));
+				}
+			}
+			else
+			{
+				if (board.getCharAt(c.getX(), c.getY() - 1) == ' ')
+				{
+					return new PlayablePair(c, new Coord(c.getX(), c.getY() - 1));
+				}
+				else 
+				{
+					return new PlayablePair(c, new Coord(c.getX(), c.getY() + 1));
+				}
+			}
+		}
+		catch (Exception e){ return null; }
+	}
+	
 	public int alphaBeta(Node node, int alpha, int beta, int depth, boolean maximizingPlayer) // Max should always be O's when playing against a person
 	{
 		int v = -1;
@@ -60,6 +90,7 @@ public class Minimax {
 				//Check if child will produce a playable pair
 				//If it does, remove both coordinates from list of available moves and make recursive call
 				//Otherwise continue with loop
+				
 				
 				
 				v = Math.max(v, alphaBeta(child, alpha, beta, depth - 1, false));
